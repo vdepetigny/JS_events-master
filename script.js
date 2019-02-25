13a66680b728a9bf4af6dfa121ac582c27216be6
vdepetigny <vdepetigny@yahoo.fr>
2019-02-25 12:43:49 +0100

    ajout script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Fonctionnalité 1 :
+
+// Fonctionnalité 2 :
+
+// Fonctionnalité 3 :
+
+// Fonctionnalité 4 :
+
+// Fonctionnalité 5 :
+
+// Fonctionnalité 6 :
+
+// Fonctionnalité 7 :
+
+// Fonctionnalité 8 :
